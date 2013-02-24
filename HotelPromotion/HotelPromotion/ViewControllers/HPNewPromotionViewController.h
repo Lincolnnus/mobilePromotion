e3fc1abcb8e61a24e6b95e470024d133bea23580
lincolnnus <shaohuan.li@gmail.com>
2013-02-25 02:20:44 +0800

    Added Hotel Promotion Module

---
+++ HotelPromotion/HotelPromotion/ViewControllers/HPNewPromotionViewController.h
@@ -0,0 +1,18 @@
+//
+//  HPNewPromotionViewController.h
+//  HotelPromotion
+//
+//  Created by Le Viet Tien on 24/2/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface HPNewPromotionViewController : BaseViewController
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+@property (weak, nonatomic) IBOutlet UITextView *contentTextView;
+@property (weak, nonatomic) IBOutlet UITextField *categoryTextField;
+@property (weak, nonatomic) IBOutlet UITextField *capacityTextField;
+@property (strong, nonatomic) IBOutlet UITextField *startTimeTextField;
+@property (strong, nonatomic) IBOutlet UITextField *endTimeTextField;
+@end
